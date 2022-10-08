eee8c626611b9bc291cfb949680b741e19d6a09c
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-10-08 16:18:33 +0300

    Frontend

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request, send_from_directory
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+#app = Flask(__name__)
+app = Flask(__name__,static_folder='./frontend/build',static_url_path='')
+
+@cross_origin()
+@app.route("/api/model", methods=["get","post"])
+def model():
+    data = request.json
+    return jsonify({'status': 200}) 
 
 @app.route("/")
-def index():
-    return "hello"
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
